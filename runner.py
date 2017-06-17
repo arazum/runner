413fcc4d191a182420f97b5b990534d0ffc70141
Antun Razum <antun.razum@gmail.com>
2017-06-17 18:52:30 +0200

    Added help.

--- runner.py
+++ runner.py
@@ -77,19 +77,19 @@ def command_list(args):
         print ('{} {}\n'.format(job['command'], job['args']))
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-q', '--queue', default=DEFAULT_QUEUE_FILE, metavar='FILE')
-    subparsers = parser.add_subparsers()
+    parser = argparse.ArgumentParser(description='''Run blocking jobs. When a job is ran its environment is taken from the host process (run subcommand) and working directory from where the job was added (add subcommand).''')
+    parser.add_argument('-q', '--queue', default=DEFAULT_QUEUE_FILE, metavar='FILE', help='''Queue file to use in subcommands. Default is '.queue' in current directory.''')
+    subparsers = parser.add_subparsers(help='''Available subcommands. Use 'runner.py {subcommand} --help' for more details.''')
 
-    parser_r = subparsers.add_parser('run')
+    parser_r = subparsers.add_parser('run', help='''Start runner host. The runner is reading jobs from the specified queue file (see -q flag).''')
     parser_r.set_defaults(func=command_run)
 
-    parser_a = subparsers.add_parser('add')
+    parser_a = subparsers.add_parser('add', help='''Add a new job to the queue. Job will be run with the working directory from which it was added. Job is added to the specified queue file (see -q flag).''')
     parser_a.set_defaults(func=command_add)
-    parser_a.add_argument('command')
-    parser_a.add_argument('args', nargs=argparse.REMAINDER)
+    parser_a.add_argument('command', help='command to run')
+    parser_a.add_argument('args', nargs=argparse.REMAINDER, help='command arguments')
 
-    parser_l = subparsers.add_parser('list')
+    parser_l = subparsers.add_parser('list', help='''List queued jobs.''')
     parser_l.set_defaults(func=command_list)
 
     args = parser.parse_args()
